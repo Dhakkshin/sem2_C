7d4ac102aeb2f88a7dcfee50a99ad2e251e4c436
unknown <srdhakkshin04@gmail.com>
2023-06-28 07:16:16 +0530

    28/06/23

--- structs/store2.c
+++ structs/store2.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
-void insert(struct item items[], int n);
-struct item ithElement(struct item[], int i);
-void printAll(struct item items[]);
-struct item search(struct item items[], char name[]);
-struct item*  priceGreaterThan(struct item items[], int n);
+#include <stdlib.h>
 
 struct item
 {
@@ -13,47 +8,56 @@ struct item
     int quantity, price;
 };
 
-int length = 0, len;
+void insert(struct item items[], int n);
+struct item ithElement(struct item[], int i);
+void printAll(struct item items[]);
+int search(struct item items[], char name[]);
+struct item*  priceGreaterThan(struct item items[], int n);
+
+int length = 1, len;
 
 int main(void)
 {
-    struct item items[100];
+    struct item* items = malloc(sizeof(struct item));
+    items[0] = (struct item){"sample", 0, 0};
 
-    int lenght = 0, choice, n, exit = 0;
-
+    int length = 0, choice, n, exit = 0;
 
     do
     {    
-    printf("1. Insert\n 2. ith item\n 3. Search\n 4. Greater than n5. Print all\n 6.Exit\n");
-    scanf("%i", choice);
-    fflush(stdin);
+    printf("1. Insert\n2. ith item\n3. Search\n4. Greater than n\n5. Print all\n6.Exit\n");
+    scanf("%i", &choice);
+    getchar();
     switch (choice)
     {
         case 1:
         printf("Enter the number of elemnets you want to add: ");
-        scanf("%i", n);
+        scanf("%i", &n);
+        getchar();
         insert(items, n);
         break;
 
         case 2:
         printf("Enter the index of the item you want to access: ");
-        scanf("%i", n);
+        scanf("%i", &n);
+        getchar();
         struct item x = ithElement(items, n);
-        printf("%s %i %i\n", x.name, x.quantity, x.price);
+        printf("Item: %s\tQuantity: %d\tUnit price:%d\n", x.name, x.quantity, x.price);
         break;
 
         case 3:
         printf("Enter the name of the item you want to search: ");
         char itemToSearch[100];
         fgets(itemToSearch, 100, stdin);
-        struct item x = search(items, itemToSearch);
-        printf("%s %i %i\n", x.name, x.quantity, x.price);
+        int y = search(items, itemToSearch);
+        (y == 1) ? printf("Item found.\n") : printf("Item not found.\n");
         break;
 
         case 4:
         printf("Enter a number: ");
-        scanf("%i", n);
-        struct item*  tmp = priceGreaterThan(struct item items[], int n);
+        scanf("%i", &n);
+        getchar();
+        struct item*  tmp = priceGreaterThan(items, n);
         printf("The following have price greater than n: \n");
         for (int i = 0; i < len; i++)
         {
@@ -70,11 +74,11 @@ int main(void)
         break;
 
         default:
-        printf("Invalid Choice.");
+        printf("Invalid Choice.\n");
         break;
     }
     }while(exit != 1);
-
+    return 0;
 }
 
 // add, ithItem, search, greater than n
@@ -82,22 +86,29 @@ int main(void)
 void insert(struct item items[], int n)
 {
     int q, p;
-    for (int i = length; i < length - 1 + n; i++)
+    struct item* tmp = realloc(items, (length + n) * sizeof(struct item));
+    if (tmp == NULL)
+    {
+        printf("Error reallocating memory./n");
+        exit(1);
+    }
+    items = tmp;
+    for (int i = length; i < length + n; i++)
     {
         printf("Enter item name: ");
         fgets(items[i].name, 100, stdin);
         
         printf("Enter quantity: ");
         scanf("%i", &q);
+        getchar();
         items[i].quantity = q;
-        fflush(stdin);
         
         printf("Enter price: ");
         scanf("%i", &p);
-        fflush(stdin);
-        items[i].price = q;
+        getchar();
+        items[i].price = p;
     }
-    length++;
+    length = length + n;
 }
 
 struct item ithElement(struct item items[], int i)
@@ -105,15 +116,16 @@ struct item ithElement(struct item items[], int i)
     return items[i];
 }
 
-struct item search(struct item items[], char name[])
+int search(struct item items[], char name[])
 {
     for (int i = 0; i < length; i++)
     {
         if (strcmp(items[i].name, name) == 0)
         {
-            return items[i];
+            return 1;
         }
     }
+    return 0;
 }
 
 void printAll(struct item items[])
@@ -126,14 +138,23 @@ void printAll(struct item items[])
 
 struct item*  priceGreaterThan(struct item items[], int n)
 {
-    struct item greaterThann[100];
-    len = 0;
+    struct item* greaterThann = malloc(sizeof(struct item));
+    greaterThann[0] = (struct item){"sample", 0, 0};
+    len = 1;
     for(int i = 0; i < length; i++)
     {
         if (items[i].price > n)
         {
-            greaterThann[len] = items[i];
             len++;
+            struct item* tmp = realloc(greaterThann, len * sizeof(struct item));
+            if (tmp == NULL)
+            {
+                printf("Error reallocating memory.\n");
+                exit(1);
+            }
+            greaterThann = tmp;
+            greaterThann[len - 1] = items[i];
+            
         }
     }
     return greaterThann;
