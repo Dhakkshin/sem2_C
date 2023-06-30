5a7087217155efe5b406f1fd548cebac3ebfaf42
unknown <srdhakkshin04@gmail.com>
2023-06-30 09:16:14 +0530

    30/06/23

--- assignmentPresentation/Q1.c
+++ assignmentPresentation/Q1.c
@@ -3,22 +3,26 @@ integers.*/
 
 #include <stdio.h>
 
-int power(int x, int n);
+float power(int x, int n);
 
 int main(void)
 {
     int x, n;
     printf("Enter x,n : ");
-    scanf("%i**%i", &x, &n);
+    scanf("%i^%i", &x, &n);
 
-    printf("%i**%i = %i\n", x, n, power(x, n));
+    printf("%i^%i = %f\n", x, n, power(x, n));
 }
 
-int power(int x, int n)
+float power(int x, int n)
 {
     if (n == 0)
     {
         return 1;
+    }
+    else if ( n < 0)
+    {
+        return 1 / power(x, -n);
     }
     else
     {
